4a958d55149333f07dc1d094becaa089cc5afa1a
ImoA-InoA <76636720+ImoA-InoA@users.noreply.github.com>
2021-03-27 15:25:46 +0900

    テストコードの修正４

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -93,49 +93,49 @@ RSpec.describe User, type: :model do
       it 'last_nameは漢字・平仮名・カタカナ以外では登録できないこと' do
         @user.last_name = 'ＡＢＣ１２３'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last_name confirmation doesn't match Last_name")
+        expect(@user.errors.full_messages).to include("Last name is invalid")
       end
 
       it 'first_nameは漢字・平仮名・カタカナ以外では登録できないこと' do
         @user.first_name = 'ＡＢＣ１２３'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First_name confirmation doesn't match First_name")
+        expect(@user.errors.full_messages).to include("First name is invalid")
       end
 
       it 'last_name_kanaは全角カタカナ以外では登録できないこと' do
         @user.last_name_kana = 'abc123'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last_name_kana confirmation doesn't match Last_name_kana")
+        expect(@user.errors.full_messages).to include("Last name kana is invalid")
       end
 
       it 'first_name_kanaは全角カタカナ以外では登録できないこと' do
         @user.first_name_kana = 'abc123'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First_name confirmation doesn't match First_name_kana")
+        expect(@user.errors.full_messages).to include("First name kana is invalid")
       end
 
       it 'emailは＠を含まなければ登録できない' do
         @user.email = 'qwerrtyui'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Email can't be blank")
+        expect(@user.errors.full_messages).to include("Email is invalid")
       end
 
       it 'passwordは英語のみでは登録できないこと' do
         @user.password = 'abcdefg'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password can't be blank")
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
 
       it 'passwordは数字のみでは登録できないこと' do
         @user.password = '1234567'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password can't be blank")
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
 
       it 'passwordは全角では登録できないこと' do
         @user.password = 'abc4567'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password can't be blank")
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
 
     end
