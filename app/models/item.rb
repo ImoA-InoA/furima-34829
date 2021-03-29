4cc487762f6707d00de89fe682e9ac92f4291265
ImoA-InoA <76636720+ImoA-InoA@users.noreply.github.com>
2021-03-29 21:11:20 +0900

    バリデーションの設定

---
+++ app/models/item.rb
@@ -0,0 +1,21 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  with_options presence: true do
+    validates :product_name
+    validates :description
+    validates :category_id
+    validates :status_id 
+    validates :burden_id
+    validates :area_id
+    validates :delivery_id
+    validates :price
+    validates :user
+  end
+  has_one_attached :image
+  belongs_to :category
+  belongs_to :status
+  belongs_to :burden
+  belongs_to :area
+  belongs_to :delivery
+end
