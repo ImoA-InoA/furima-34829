053d1c9facf8cdec6d304b5160769f96e1597228
ImoA-InoA <76636720+ImoA-InoA@users.noreply.github.com>
2021-04-02 18:01:11 +0900

    指摘箇所の修正１

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -28,10 +28,22 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Category is not a number")
       end
 
+      it 'category_idが1では登録できないこと' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+
       it 'status_idが空では登録できないこと' do
         @item.status_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Status is not a number")
+      end
+
+      it 'status_idが1では登録できないこと' do
+        @item.status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
       end
 
       it 'burden_idが空では登録できないこと' do
@@ -40,10 +52,22 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Burden is not a number")
       end
 
+      it 'burden_idが1では登録できないこと' do
+        @item.burden_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Burden must be other than 1")
+      end
+
       it 'area_idが空では登録できないこと' do
         @item.area_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Area is not a number")
+      end
+
+      it 'area_idが1では登録できないこと' do
+        @item.area_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area must be other than 1")
       end
 
       it 'delivery_idが空では登録できないこと' do
@@ -52,11 +76,54 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Delivery is not a number")
       end
 
+      it 'delivery_idが1では登録できないこと' do
+        @item.delivery_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery must be other than 1")
+      end
+
       it 'priceが空では登録できないこと' do
         @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
+
+      it 'priceは全角文字では登録できないこと' do
+        @item.price = '１２３４５'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is invalid")
+      end
+
+      it 'priceは半角英数混合では登録できないこと' do
+        @item.price = 'abc123'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is invalid")
+      end
+
+      it 'priceは半角英語だけでは登録できないこと' do
+        @item.price = 'abcdefg'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is invalid")
+      end
+
+      it 'priceは299円以下では登録できないこと' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is invalid")
+      end
+
+      it 'priceは10,000,000以上では登録できないこと' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is invalid")
+      end
+
+      it 'imageが空では登録できないこと' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is invalid")
+      end
+
     end
   end
 end
