fe4902b31f747af7dc646e6149b06676095caa92
ImoA-InoA <76636720+ImoA-InoA@users.noreply.github.com>
2021-04-04 11:49:16 +0900

    指摘箇所の修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,15 +22,15 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def edit
-    @item = Item.find(params[:id])
-  end
+  # def edit
+    # @item = Item.find(params[:id])
+  # end
 
-  def destroy
-    @item = Item.find(params[:id])
-    @item.destroy
-    redirect_to @item
-  end
+  # def destroy
+    # @item = Item.find(params[:id])
+    # @item.destroy
+    # redirect_to @item
+  # end
 
   private
   def item_params
