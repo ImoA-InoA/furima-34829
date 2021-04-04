094618c6eae075e31d47077bb7a7d5b2e9b7c41f
ImoA-InoA <76636720+ImoA-InoA@users.noreply.github.com>
2021-04-04 18:49:43 +0900

    指摘箇所の修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,15 +21,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+
   end
 
   def edit
-    @item = Item.find(params[:id])
+    
   end
 
   def update
-    @item = Item.find(params[:id])
+    
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -38,7 +38,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find(params[:id])
+    
     @item.destroy
     redirect_to root_path
   end
